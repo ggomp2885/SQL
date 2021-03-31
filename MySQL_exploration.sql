09d839b3fafeca69665d940d5eaf2a7991654ec7
Greg Gompers <ggompers28@gmail.com>
2021-03-31 13:05:47 -0400

    Add - Delimiter changing function

--- MySQL_exploration.sql
+++ MySQL_exploration.sql
@@ -237,7 +237,7 @@
 --                 message VARCHAR(100)
 -- );
 -- USE trig_explore;
--- DELIMITER $$         -- must change delimiter for triggers
+-- DELIMITER $$         -- must change delimiter for triggers 
 -- CREATE TRIGGER my_trigger BEFORE INSERT
 --     ON employee
 --     FOR EACH ROW BEGIN
@@ -257,7 +257,7 @@
 
 
 
-                        -- Dropping a trigger 
+                        -- Dropping a trigger
 -- USE trig_explore;
 -- DROP TRIGGER IF EXISTS trig_explore.my_trigger;
 
